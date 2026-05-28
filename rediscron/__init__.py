59aca21db680dc2ee78b32b1e2314aec793b5a14
Stefan 'hr' Berder <stefan@measureofquality.com>
2026-05-28 23:56:54 +0700

    feat: add dynamic version managed by hatchling

--- rediscron/__init__.py
+++ rediscron/__init__.py
@@ -1,5 +1,5 @@
 """Redis-backed runtime-editable cron scheduler for RQ."""
-
+from .__meta__ import __version__
 from .core import RedisCronJob, RedisCronScheduler
 
 __all__ = ["RedisCronJob", "RedisCronScheduler"]
---
+++ rediscron/__meta__.py
@@ -0,0 +1,3 @@
+__author__ = 'Stefan Berder <stefan@measureofquality.com>'
+__contact__ = 'code+rediscron@measureofquality.com'
+__version__ = "0.1.2"
